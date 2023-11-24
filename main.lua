074edeba1a0d24a12537a464ec205ee5a345f433
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-11-24 10:27:37 +0100

    [demo] click to display next test

--- main.lua
+++ main.lua
@@ -10,6 +10,13 @@ end -- see docs of extension: tomblind.local-lua-debugger-vscode
 
 require("draw_primitives_testing")
 
+test_index = 3
 function display()
-    draw_primitives_test3()
+    local tests = { draw_primitives_test1, draw_primitives_test2, draw_primitives_test3 }
+
+    if pointer_click_check() then
+        test_index = (((test_index - 1) + 1) % #tests) + 1
+    end
+
+    tests[test_index]()
 end
