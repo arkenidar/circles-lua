fac60a222381366389587f0908dfb437ee8bd917
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-11-26 02:15:32 +0100

    add from github.com/arkenidar/love2d-algorithms

--- utils_math.lua
+++ utils_math.lua
@@ -5,3 +5,11 @@ end
 function distance(point1, point2)
     return math.sqrt((point1[1] - point2[1]) ^ 2 + (point1[2] - point2[2]) ^ 2)
 end
+
+function point_inside_rectangle(point, rectangle)
+    return
+        point[1] >= rectangle[1] and
+        point[1] <= (rectangle[1] + rectangle[3]) and
+        point[2] >= rectangle[2] and
+        point[2] <= (rectangle[2] + rectangle[4])
+end
